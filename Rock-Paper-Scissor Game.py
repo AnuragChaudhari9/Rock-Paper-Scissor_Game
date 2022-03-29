fab2e80690171ae2d5863eae3f21afeeaeaad6e5
AnuragChaudhari9 <100065211+AnuragChaudhari9@users.noreply.github.com>
2022-03-29 17:40:44 +0530

    Update Rock-Paper-Scissor Game.py

--- Rock-Paper-Scissor Game.py
+++ Rock-Paper-Scissor Game.py
@@ -1,15 +1,5 @@
 import random
 
-# def rps(comp,mine):
-#     if (comp == 'r' and mine == 'p'):
-#         return "YouWin"
-#     elif (comp == 'p' and mine == 's'):
-#         return "YouWin"
-#     elif (comp == 's' and mine == 'r'):
-#         return "YouWin"
-#     elif (comp == mine):
-#         return "Tie"
-
 def rps(comp,mine):
     if (comp == 'r' and mine == 'p'):
         return True
@@ -41,11 +31,4 @@ elif win == None:
     print("Tied!")
 else:
     print("Haha! Better luck next time!")
-
-# if (rps(comp,mine) == "YouWin"):
-#     print("You Won!")
-# elif (rps(comp,mine) == "Tie"):
-#     print("Tied!")
-# else:
-#     print("You Lose!")
 
